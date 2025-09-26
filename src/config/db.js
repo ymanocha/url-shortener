4d80e1348c91d95ca88d6e0327ab7e8dd5d299f1
ymanocha <yashmanocha19@gmail.com>
2025-09-27 02:15:42 +0530

    connected db to supabase

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    idleTimeoutMillis: 30000,           
+    connectionTimeoutMillis: 2000
 });
 
 module.exports = pool;
