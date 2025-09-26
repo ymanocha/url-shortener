52702108cb33d1df891a3f5040878774cfecc2e2
ymanocha <yashmanocha19@gmail.com>
2025-09-27 00:09:56 +0530

    Added get,update,delete,access functionality

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,6 +1,6 @@
 const pool = require('../config/db')
 const {encode,decode} = require('../utils/shortCode')
-
+const {StatusCodes} = require('http-status-codes')
 
 const createShort = async (req,res)=>{
   const {url} = req.body
@@ -11,6 +11,7 @@ const createShort = async (req,res)=>{
     [url]
   );
   const id = result.rows[0].id;
+  
   const shortcode = encode(id);
   await pool.query(
     `UPDATE urls
@@ -18,19 +19,78 @@ const createShort = async (req,res)=>{
      WHERE id = $2`,
      [shortcode, id]
   )
-  res.json({id,url,shortcode});
+  const print = await pool.query (
+      `SELECT *
+      FROM urls
+      WHERE shortcode = $1`,
+      [shortcode]
+  )
+
+  res.status(StatusCodes.CREATED).json(print.rows[0]);
 }
 
-const getShort = async () =>{
-    res.send('ej')
+const getShort = async (req,res) =>{
+   const {params:{id:shortcode}} = req
+        const result = await pool.query(
+            `SELECT * 
+            FROM urls
+            WHERE shortcode = $1`,
+            [shortcode]
+        )
+        const {id,accesscount, url} = result.rows[0];
+        
+        await pool.query(
+          `UPDATE urls
+           SET accesscount = $1,
+           updatedat = NOW()
+           WHERE shortcode = $2`,
+           [Number(accesscount)+1,shortcode]
+        )
+        res.redirect(302, url)
+    }
+
+
+const updateShort = async (req,res) =>{
+    const {params:{id:shortcode},body:{url}} = req
+     
+    
+
+    await pool.query(
+      `UPDATE urls
+      SET url = $1, updatedat = NOW()
+      WHERE shortcode = $2`,
+      [url,shortcode]
+    )
+    const result = await pool.query(  
+      `SELECT * 
+      FROM urls
+      WHERE shortcode = $1`,
+      [shortcode]
+    ) 
+    
+    res.json(result.rows)
 }
 
-const updateShort = async () =>{
-    res.send('ej') 
-}
+const deleteShort = async (req,res) =>{
+    const {params:{id:shortcode}} = req
+    
+    await pool.query(
+      `DELETE FROM urls
+       WHERE shortcode = $1 `,
+       [shortcode]
+      )
 
-const deleteShort = async () =>{
-    res.send('ej') 
+    res.status(StatusCodes.NO_CONTENT).send()
 }
+const getURLstats = async (req,res) =>{
+  const {params:{id:shortcode}} = req
+  const result = await pool.query(
+    `SELECT * 
+    FROM urls
+    WHERE shortcode = $1`,
+    [shortcode]
+  )
+  res.status(200).json(result.rows[0])
 
-module.exports = {createShort, getShort, updateShort, deleteShort}
+}
+module.exports = {createShort, getShort, updateShort, deleteShort, getURLstats}
--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {createShort, getShort, updateShort, deleteShort} = require('../controllers/urlController')
+const {createShort, getShort, updateShort, deleteShort, getURLstats} = require('../controllers/urlController')
 
 router.route('/').post(createShort)
 router.route('/:id').get(getShort).put(updateShort).delete(deleteShort)
-
+router.route('/:id/stats').get(getURLstats)
 module.exports = router;
